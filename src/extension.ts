89e8c576c91a0a3b98be7565b1a43537815c34fc
x <x@live.com>
2022-09-17 18:01:06 +0300

    as is

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,43 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { ExtensionContext, StatusBarAlignment, window, env, StatusBarItem, Selection, workspace, TextEditor, commands } from 'vscode';
+import {
+	ExtensionContext,
+	StatusBarAlignment,
+	window,
+	env,
+	StatusBarItem,
+	Selection,
+	workspace,
+	TextEditor,
+	commands,
+	SignatureHelp,
+	SignatureHelpProvider as ISignatureHelpProvider,
+	SignatureInformation
+
+} from 'vscode';
 import { basename } from 'path';
 // const fse = require('fs-extra')
 import { readFileSync } from 'fs-extra';
 import { posix } from 'path';
 import { TextDecoder } from 'util';
 
+// import {
+// 	ExtensionContext, languages,
+// 	SignatureHelp,
+// 	SignatureHelpProvider as ISignatureHelpProvider,
+// 	SignatureInformation
+// } from 'vscode';
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
 enum SolitityType {
 	FUNCTION,
-	MODIFIER
+	MODIFIER,
+
+
 }
 
 
@@ -30,10 +54,17 @@ interface solidityIdentifier {
 	interface: string;
 }
 
+interface SolidityVariableInfo {
+	type:  string,
+	name: string,
+	line: string
+}
+
 
 let clipboard = '';
 let contractName2ContractText: Record<string, string> = {}
 let contractName2FunctionsList: Record<string, string[]> = {}
+let contractName2VariablesList: Record<string, SolidityVariableInfo[]> = {}
 
 
 
@@ -49,11 +80,39 @@ let getFunctionsListFromDocumentForTestingAsString = () => {
 	return functions;
 }
 
+let getPublicFromDocumentForTestingAsString = () => {
+	let editor = window.activeTextEditor;
+	let docText = editor?.document.getText() ?? '';
+	// let re = /^\s+(.*?)\s+(?<visibility>public|private)\s*(constant)?\s*(?<name>.+?)(\s+|;)/gm;
+	let re = /^\s+([^\/]*?)\s+(?<visibility>public|private)?\s*/gm;
+	let arr1 = [...docText?.matchAll(re)].map(e => e[1].replace(/\s+/g, ' ').replace(/^/gm, '- [ ] ').replace(/$/gm, ';'))
+	console.log('arr1 is ');
+	console.log(arr1.length);
+	console.log(arr1);
+	let functions = arr1.join("\n") + "\n";
+	return functions;
+}
+
+
 let getFunctionNamesList = (text: string) => {
 	let re = /(function\s+(.+?)\([\s\S]*?\)[\s\S]+?)\s*\{/g;
 	let arr1 = [...text?.matchAll(re)].map(e => e[2].replace(/\s+/gm, '')); // .replace(/^/gm, '- [ ] ').replace(/$/gm, ';'))
 	return arr1;
 }
+
+let getPublicParameters = (text: string) => {
+	let re = /^\s*(?<type>((mapping\(.+?\))|address|uint\d+|string|[A-z]+)([])?) ?(?<visibility>public|private|external|internal)? ?(?:(constant|immutable) )?(?<name>([a-zA-Z0-9_]*))\s*(=.*)?;/gm;
+	let arr1 = [...text?.matchAll(re)]; // .map(e => e[2].replace(/\s+/gm, '')); // .replace(/^/gm, '- [ ] ').replace(/$/gm, ';'))
+	console.log('salamt');
+
+	// contractName2VariablesList.
+	
+	// return arr1;
+
+	// const { groups: { token } } = /Bearer (?<token>[^ $]*)/.exec()
+	// console.log(token)
+}
+
 
 let copyToClipboard = (text: string) => {
 	// let wordToCopy = functions;
@@ -96,7 +155,7 @@ let injectContractsFromContractsFolder = async () => {
 			if (name.endsWith('.sol') === false) continue;
 			let name2 = name.replace(/\.sol$/, '').toLowerCase();
 			const filePath = posix.join(path.fsPath, name);
-			const stat = await vscode.workspace.fs.stat(path.with({ path: filePath }));
+			// const stat = await vscode.workspace.fs.stat(path.with({ path: filePath }));
 			console.log('file path is ');
 			console.log(filePath);
 			const sampleTextEncoded = await vscode.workspace.fs.readFile(vscode.Uri.file(filePath));
@@ -114,6 +173,16 @@ let extractContractFunctionFromContractsTextMap = () => {
 		contractName2FunctionsList[name] = arr1;
 	}
 }
+
+
+let extractContractVariablesFromContractsTextMap = () => {
+	for (const [name, text] of Object.entries(contractName2ContractText)) {
+		let re = /(function\s+(.+?)\([\s\S]*?\)[\s\S]+?)\s*\{/g;
+		let arr1 = [...text?.matchAll(re)].map(e => e[2].replace(/\s+/gm, '')); // .replace(/^/gm, '- [ ] ').replace(/$/gm, ';'))
+		contractName2FunctionsList[name] = arr1;
+	}
+}
+
 
 let initContractsData = async () => {
 	await injectContractsFromContractsFolder();
@@ -181,6 +250,11 @@ export async function activate(context: vscode.ExtensionContext) {
 	);
 
 	context.subscriptions.push(contractFunctionsProvider, disposableCopyFns, disposableImportContracts);
+
+	// TODO add signatures here
+
+
+
 }
 
 // this method is called when your extension is deactivated
