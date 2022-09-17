f4c5eaa1cbc0ba721f2ee4ad895074168c8d6848
x <x@live.com>
2022-09-17 11:56:25 +0300

    as is

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,42 @@ import { ExtensionContext, StatusBarAlignment, window, env, StatusBarItem, Selec
 import { basename } from 'path';
 // const fse = require('fs-extra')
 import { readFileSync } from 'fs-extra';
+import { posix } from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+enum SolitityType {
+	FUNCTION,
+	MODIFIER
+}
+
+
+enum SolitityVisibility {
+	public,
+	private,
+}
+
+interface solidityIdentifier {
+	type: SolitityType,
+	interface: string;
+}
+
+// let getSolidityIdentifiers = (text: string) => {
+// 	let re = /((function|modifier)\s+.+?\([\s\S]*?\)[\s\S]+?)\s*\{/g;
+// 	let arr1 = [...text.matchAll(re)].map(e => e[1].replace(/\s+/g, ' ').replace(/^/gm, '- [ ] ').replace(/$/gm, ';'));
+// 	let arr2 = arr1.map(e => {
+// 		let m : SolitityType = 
+// 		let obj: solidityIdentifier = { interface: e, type: SolitityType.FUNCTION };
+// 		return {};
+// 	})
+
+// }
+
+
+
+
+
 let getFunctionsListFromDocumentForTestingAsString = () => {
 	let editor = window.activeTextEditor;
 	let docText = editor?.document.getText() ?? '';
@@ -63,22 +96,56 @@ let copyToClipboard = (text: string) => {
 
 let clipboard = '';
 
-let readContracts = () => {
-	let m = readFileSync('./contracts/Vault.sol');
-	console.log(m);
+let readContracts = async () => {
+	// let m = readFileSync('./contracts/Vault.sol');
+	// let m = await vscode.workspace.fs.readDirectory(vscode.Uri.parse(vscode.workspace.asRelativePath('.', true)));
+	// let m = await vscode.workspace.fs.readDirectory(vscode.Uri.));
+	// console.log(m);
+
+	if (vscode.workspace.workspaceFolders == null) return;
+	const workspaceFolderUri = vscode.workspace.workspaceFolders![0].uri;
+	const folderPath = posix.relative(workspaceFolderUri.path, '/contracts/')
+	console.log(folderPath);
+
+	// window.activeTextEditor.document.uri;
+	// const folderPath0 = posix.dirname(fileUri.path);
+	// const folderPath = posis.relative(folderPath0, '../contracts/')
+	// const folderUri = fileUri.with({ path: folderPath });
+	// const doc = await vscode.workspace.openTextDocument({ content: "salamatooo" });
+
+	// const p1 = vscode.window.activeTextEditor.document.uri.path;
+	// const p2 = vscode.window.activeTextEditor.document.uri.path;
+	// console.log(p1);
 }
 
-export function activate(context: vscode.ExtensionContext) {
-	console.log('salamat');
+export async function activate(context: vscode.ExtensionContext) {
+	// try {
+
+	// 	readContracts();
+	// 	console.log('salamat');
+	// } catch (error) {
+	// 	console.log('salamat');
+
+	// }
+	// console.log('salamat');
+	// // }
+
+	// // export async function activate(context: vscode.ExtensionContext) {
+	console.log('salamat0');
 	try {
-		readContracts();
+		const workspaceFolderUri = vscode.workspace.workspaceFolders![0].uri;
+		const folderPath = posix.relative(workspaceFolderUri.path, '/contracts/')
+		console.log('salamat=============');
+		console.log(folderPath);
+		console.log('salamatoooo=================');
+		// await readContracts();
 	} catch (error) {
 		console.log('error is ');
 		console.log(error);
 	}
 	console.log('done');
 
-	console.log('sk it is activateddddd ');
+	console.log('sk it is activateddddd 0');
 
 	let disposableCopyFns = vscode.commands.registerCommand('sk-blockchain-helper.copy-functions', () => {
 		let fns = getFunctionsListFromDocumentForTestingAsString();
@@ -89,6 +156,8 @@ export function activate(context: vscode.ExtensionContext) {
 		let text = await vscode.env.clipboard.readText();
 		clipboard = text;
 	});
+
+
 
 
 
