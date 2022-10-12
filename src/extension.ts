61bcb28fb71c946b3e1de88b4d344d7a1b777f95
x <x@live.com>
2022-10-12 23:10:19 +0300

    as is

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,35 @@
 import * as vscode from 'vscode';
 
-const disposablePasteTextAsSnippet = vscode.commands.registerCommand('sk-vscode-helper.paste-snippets-template', () => {
-
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) return;
-
-	vscode.env.clipboard.readText().then((text) => {
-
-		let str = text;
-		str = str.replace(/(?:\r\n|\r|\n)/g, '\\\\n');
-
-		editor.edit(editBuilder => {
-			editBuilder.insert(editor.selection.active, str);
-		});
-	});
-});
 
 export async function activate(context: vscode.ExtensionContext) {
+
+	const disposablePasteTextAsSnippet = vscode.commands.registerCommand('sk-vscode-helper.paste-snippets-template', () => {
+
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+
+		vscode.env.clipboard.readText().then((text) => {
+
+			let str = text;
+			str = str.replace(/(?:\r\n|\r|\n)/g, '\\\\n');
+
+			editor.edit(editBuilder => {
+				editBuilder.insert(editor.selection.active, str);
+			});
+		});
+	});
+
 	context.subscriptions.push(
 		disposablePasteTextAsSnippet,
 	);
+
+	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World!');
+	});
+
+	context.subscriptions.push(disposable);
 }
+
+
 
 export function deactivate() { }
