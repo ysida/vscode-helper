6f80722bde929e3635f68150c81e5816feffc418
x <x@live.com>
2022-10-12 23:03:07 +0300

    as is

--- src/extension.ts
+++ src/extension.ts
@@ -3,32 +3,21 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as fse from 'fs-extra';
 
-import {
-	window,
-	env,
-	SignatureHelp,
-	SignatureHelpProvider as ISignatureHelpProvider,
-	SignatureInformation
-
-} from 'vscode';
-import { posix } from 'path';
-import { TextDecoder } from 'util';
 
 
 const disposablePasteTextAsSnippet = vscode.commands.registerCommand('sk-vscode-helper.copy-functions', () => {
+
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) return;
+
 	vscode.env.clipboard.readText().then((text) => {
-
-
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) return;
 
 		let str = text;
 		str = str.replace(/(?:\r\n|\r|\n)/g, '\\\\n');
 
 		editor.edit(editBuilder => {
-			editBuilder.insert(editor.selection.active, text);
+			editBuilder.insert(editor.selection.active, str);
 		});
 
 	});
